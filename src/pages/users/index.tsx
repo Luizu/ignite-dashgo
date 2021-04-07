c39810726b72c82a5878d220cda788535ae96b0b
luizu <luizucastilho@gmail.com>
2021-04-06 23:56:52 -0300

    Chore: Add responsitity to list users page

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,10 +1,15 @@
-import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text, useBreakpointValue } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true
+  })
+
   return (
     <Box>
       <Header />
@@ -24,16 +29,20 @@ export default function UserList() {
           <Table colorScheme="whiteAlpha">
             <Thead>
               <Tr>
-                <Th px="6" color="gray.300" w="8">
+                <Th px={["4", "4", "6"]} color="gray.300" w="8">
                   <Checkbox colorScheme="pink" />
                 </Th>
                 <Th>Usúario</Th>
-                <Th>Data de cadastro</Th>
-                <Th w="8"></Th>
+                {isWideVersion &&
+                  <>
+                    <Th>Data de cadastro</Th>
+                    <Th w="8"></Th>
+                  </>
+                }
               </Tr>
             </Thead>
             <Tbody>
-              <Td px="6">
+              <Td px={["4", "4", "6"]}>
                 <Checkbox colorScheme="pink" />
               </Td>
               <Td>
@@ -42,14 +51,63 @@ export default function UserList() {
                   <Text fontSize="small" color="gray.300">luizucastilho@gmail.com</Text>
                 </Box>
               </Td>
-              <Td>
-                04 de Abril, 2021
+              {isWideVersion &&
+                <>
+                  <Td>
+                    04 de Abril, 2021
+                  </Td>
+                  <Td>
+                    <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>
+                      Editar
+                    </Button>
+                  </Td>
+                </>}
+            </Tbody>
+
+            <Tbody>
+              <Td px={["4", "4", "6"]}>
+                <Checkbox colorScheme="pink" />
               </Td>
               <Td>
-                <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>
-                  Editar
-            </Button>
+                <Box>
+                  <Text fontWeight="bold">Luiz Castilho</Text>
+                  <Text fontSize="small" color="gray.300">luizucastilho@gmail.com</Text>
+                </Box>
               </Td>
+              {isWideVersion &&
+                <>
+                  <Td>
+                    04 de Abril, 2021
+                  </Td>
+                  <Td>
+                    <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>
+                      Editar
+                    </Button>
+                  </Td>
+                </>}
+            </Tbody>
+
+            <Tbody>
+              <Td px={["4", "4", "6"]}>
+                <Checkbox colorScheme="pink" />
+              </Td>
+              <Td>
+                <Box>
+                  <Text fontWeight="bold">Luiz Castilho</Text>
+                  <Text fontSize="small" color="gray.300">luizucastilho@gmail.com</Text>
+                </Box>
+              </Td>
+              {isWideVersion &&
+                <>
+                  <Td>
+                    04 de Abril, 2021
+                  </Td>
+                  <Td>
+                    <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>
+                      Editar
+                    </Button>
+                  </Td>
+                </>}
             </Tbody>
           </Table>
           <Pagination />
